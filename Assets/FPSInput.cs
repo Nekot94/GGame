f4c911dfc9e39f55d82ae8209d31ebebc6cf3971
Nekot94 <nekomato9@gmail.com>
2017-02-02 14:18:33 +0300

    first pathes+

--- Assets/FPSInput.cs
+++ Assets/FPSInput.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour {
 	public float speed = 9.0f;
 	public float gravity = -9.8f;
+    public float jumpSpeed = 15.0f;
+    public float minFall = -1.5f;
+
+    private float _vertSpeed;
 
 	private CharacterController _characterController;
 	// Use this for initialization
@@ -19,12 +23,23 @@ public class FPSInput : MonoBehaviour {
 		Vector3 movement = new Vector3 (deltaX * speed * Time.deltaTime, 0, 
 			deltaZ * speed * Time.deltaTime);
 		movement = Vector3.ClampMagnitude(movement, speed);
-		movement.y = gravity;
-		if (Input.GetButtonDown("Jump")){
-			Debug.Log ("Гаджи n1");
-		}
+	
+        if (_characterController.isGrounded) {
+            if (Input.GetButtonDown("Jump")) {
+                _vertSpeed = jumpSpeed;
+            }
+            else {
+                _vertSpeed = minFall;
+            }
+        } else {
+            _vertSpeed += gravity * 5 * Time.deltaTime;
+        }
+
+        movement.y = _vertSpeed * Time.deltaTime;
+
 		movement = transform.TransformDirection (movement);
 		_characterController.Move (movement);
+
 
 		//transform.Translate (deltaX * speed * Time.deltaTime, 0, 
 		//	deltaZ * speed * Time.deltaTime);
--- Assets/Fireball.cs
+++ Assets/Fireball.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Firebal : MonoBehaviour {
+public class Fireball : MonoBehaviour {
+    public float speed = 10.0f;
+    public int damage = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update() {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+        if (player != null) {
+            player.Hurt(damage);
+        }
+        Destroy(this.gameObject);
+    }
 }
+
